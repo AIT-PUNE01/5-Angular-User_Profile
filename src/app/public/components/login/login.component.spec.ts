fc289800240bbc9682714d0390dcd92caef309f9
AIT-PUNE01 <kavya252001chauhan@gmail.com>
2024-05-08 19:09:59 +0530

    Update login.component.spec.ts

--- src/app/public/components/login/login.component.spec.ts
+++ src/app/public/components/login/login.component.spec.ts
@@ -1,25 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'; //These import statements bring in the necessary modules and components for Angular testing.
+//ComponentFixture and TestBed are provided by Angular's testing framework for creating and testing Angular components.
+//LoginComponent is the component being tested. Its file path is specified as './login.component'.
 
 import { LoginComponent } from './login.component';
 
-describe('LoginComponent', () => {
+describe('LoginComponent', () => {  //defines a test suite
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
+  beforeEach(async () => {  //The beforeEach() function is a Jasmine hook that runs before each test case.
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ]
     })
-    .compileComponents();
+    .compileComponents();  //ompiles the component's template and CSS.
   });
 
-  beforeEach(() => {
+  beforeEach(() => {  // function sets up the fixture and component before each test case.
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', () => {  // test case verifies that the component is created successfully.
     expect(component).toBeTruthy();
   });
 });
+
+
+//Overall, this test file sets up a testing environment for the LoginComponent and verifies that it can be created successfully. It's a standard Angular test file structure used for unit testing Angular components.
